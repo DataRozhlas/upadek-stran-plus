a406cfb94aaba4cfe0de7c5b4d364f00f656ebda
datastory.cz <honza@datastory.cz>
2019-12-03 13:44:39 +0100

    uklid

--- analyza.py
+++ analyza.py
@@ -1,5 +1,7 @@
 #%%
 import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
 
 # %%
 data_volby = pd.read_excel("./data/parlgov.xlsx", sheet_name="election")
@@ -10,7 +12,6 @@ print(data_volby.columns)
 print(data_party.columns)
 # %%
 data = data_volby.merge(data_party, on="party_id", how="left")
-
 # %%
 chci=["Social democracy", "Conservative", "Liberal", "Communist/Socialist", "Christian democracy", "Agrarian", "Green/Ecologist"]
 data_filter=data[ (data.election_type=="parliament") & (data.election_date >= "1990-01-01" ) & (data.family_name.isin(chci))] 
@@ -18,5 +19,7 @@ data_filter=data[ (data.election_type=="parliament") & (data.election_date >= "1
 # %%
 for country in data_filter.country_name_x.unique():
     data_country = data_filter[ data_filter.country_name_x == country ]
+    #nakresli graf
+
 
 # %%
