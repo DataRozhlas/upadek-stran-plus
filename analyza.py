9b1486c26517e399c7e0fb6648bbb647dc86c1b5
Nela Krawiecova <nela.krawiecova@gmail.com>
2019-12-03 13:22:52 +0100

    filter data

---
+++ analyza.py
@@ -0,0 +1,22 @@
+#%%
+import pandas as pd
+
+# %%
+data_volby = pd.read_excel("./data/parlgov.xlsx", sheet_name="election")
+data_party = pd.read_excel("./data/parlgov.xlsx", sheet_name="party")
+
+# %%
+print(data_volby.columns)
+print(data_party.columns)
+# %%
+data = data_volby.merge(data_party, on="party_id", how="left")
+
+# %%
+chci=["Social democracy", "Conservative", "Liberal", "Communist/Socialist", "Christian democracy", "Agrarian", "Green/Ecologist"]
+data_filter=data[ (data.election_type=="parliament") & (data.election_date >= "1990-01-01" ) & (data.family_name.isin(chci))] 
+
+# %%
+for country in data_filter.country_name_x.unique():
+    data_country = data_filter[ data_filter.country_name_x == country ]
+
+# %%
