fd729cb83f12c8ac27145905853885205b2ea793
datastory.cz <honza@datastory.cz>
2019-12-10 12:33:54 +0100

    další kolo utrpení

--- analyza.py
+++ analyza.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 data_volby = pd.read_excel("./data/parlgov.xlsx", sheet_name="election")
 data_party = pd.read_excel("./data/parlgov.xlsx", sheet_name="party")
 
+#%%
 # %%
 data = data_volby.merge(data_party[['party_id', 'family_name']], on="party_id", how="left")
 #%%
@@ -17,27 +18,15 @@ data.election_date = pd.to_datetime(data.election_date, format='%Y-%m-%d')
 data['election_year'] = data.election_date.apply(lambda x: int(str(x).split('-')[0]))
 
 #%%
-chci=["Social democracy", "Conservative", "Liberal", "Communist/Socialist", "Christian democracy", "Agrarian", "Green/Ecologist"]
-# %%
-data = data[ (data.election_type == "parliament") & (data.election_year >= 1990 )] 
+chci=["Social democracy"]
+staty = ['Austria', 'Czech Republic', 'Germany', 'France', 'Hungary', 'Poland', 'Slovakia', 'Sweden', 'Netherlands']
+
+data = data[ (data.election_type == "parliament") & (data.election_year >= 1990 ) & data.family_name.isin(chci) & data.country_name.isin(staty)] 
 
 #%%
-
-#%%
-graph_data = []
-for family in data.family_name.unique():
-    family_data = []
-    for year in sorted(list(data.election_year.unique())):
-        tmp = data[ (data.election_year == year) & (data.family_name == family) ]
-        if tmp.vote_share.median() != tmp.vote_share.median():
-            family_data.append(0)
-        else:
-            family_data.append(tmp.vote_share.median())
-    graph_data.append(family_data)
-
-#%%
-plt.stackplot(sorted(list(data.election_year.unique())), graph_data, labels=list(data.family_name.unique()))
-plt.legend(loc='upper left')
-plt.show()
-
-# %%
+for country in sorted(list(data.country_name.unique())):
+    tmp = data[data.country_name == country]
+    chrt = sns.lineplot(x="election_year", y="vote_share", hue="family_name", data=tmp, ci=None)
+    chrt.set_title(country)
+    plt.legend(loc='upper left')
+    plt.show()
